77a7e6cf28d4a037c726991c5eda7552414e5794
Ubuntu <hs@DL-Linux.2n0ck03oahtuzkasntlu3un5ib.bx.internal.cloudapp.net>
2017-12-17 23:25:08 +0000

    3D CNN

--- npy_to_csv.py
+++ npy_to_csv.py
@@ -8,7 +8,7 @@ if len(sys.argv) < 3:
     print("\tpython3 npy_to_csv.py auc result.npy auc_submission.csv", file=sys.stderr)
     sys.exit(1)
 
-prediction = np.load(sys.argv[2]).astype(int)
+prediction = np.load(sys.argv[2]).astype('float32')
 if(prediction.shape != (1971, 19)):
     print("Labels file is not in the correct dimension", file=sys.stderr)
     sys.exit(1)
@@ -27,13 +27,18 @@ if sys.argv[1].lower() == "subset":
         for i in range(1971):
             label = []
             for j in range(19):
-                if prediction[i, j]:
+                if prediction[i, j] > 0.5:
                     label.append(tag_name[j])
             print("%d,%s" % (i, ' '.join(label)), file=f)
 else:
     # write to auc output file
     # add the id column
-    prediction = np.concatenate(
-        (np.arange(prediction.shape[0]).reshape((prediction.shape[0], 1)), prediction), axis=1)
-    np.savetxt(sys.argv[3], prediction, header="id,%s" %
-               ",".join(tag_name), delimiter=",", fmt='%d', comments='')
+    # prediction = np.concatenate(
+    #     (np.arange(prediction.shape[0]).reshape((prediction.shape[0], 1)), prediction), axis=1)
+    # np.savetxt(sys.argv[3], prediction, header="id,%s" %
+    #            ",".join(tag_name), delimiter=",", fmt='%.10f', comments='')
+    with open(sys.argv[3], 'w') as f:
+        print("id,%s" % ",".join(tag_name), file=f)
+        for i in range(1971):
+            strings = ['%.10f' % num for num in prediction[i]]
+            print("%d,%s" % (i, ",".join(strings)), file=f)
